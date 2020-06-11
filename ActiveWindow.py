d3b1c58ea093e666a922a9ff631d75aeeb780b00
mr-good-bye <nikitos99099@gmail.com>
2020-06-11 03:38:28 +0300

    Small GUI For Start And Stop

--- ActiveWindow.py
+++ ActiveWindow.py
@@ -4,6 +4,7 @@ import win32gui
 import time
 from threading import Thread
 import database as db
+from appJar import gui
 
 
 spisok = []
@@ -19,6 +20,7 @@ class stoppingThread:
 
     def run(self, dt):
         global spisok
+        self._running = True
         while self._running:
             name = active_window()
             a = name
@@ -35,16 +37,44 @@ def active_window():
     return psutil.Process(pid[-1]).name()
 
 
-u_in = input("Введите период обновления в секундах: ")
-delta = float(u_in)
 thread = stoppingThread()
-thr = Thread(target=thread.run, args=(delta,))
-thr.start()
-u_in = input("Введите что угодно для завершения:")
-thread.terminate()
-thr.join()
-for i in spisok:
-    db.add_time(i[0], i[1], i[2])
-db.save()
-for i in db.get():
-    print(i[0], ':', i[1])
+thr = ''
+
+
+def pressed(val):
+    global running
+    global thr
+    if val == "Start" and not running:
+        app.setLabel("run_l", 'Running')
+        app.setLabelBg("run_l", "cyan")
+        thr = Thread(target=thread.run, args=(0.5,))
+        thr.start()
+        running = True
+    elif val == "Stop" and running:
+        app.setLabel("run_l", 'Not Running')
+        app.setLabelBg("run_l", 'orange')
+        thread.terminate()
+        thr.join()
+        running = False
+        for i in spisok:
+            db.add_time(i[0], i[1], i[2])
+        db.save()
+        for i in db.get():
+            print(i[0], ':', i[1])
+
+
+app = gui()
+app.setTitle("time-tracker control")
+app.setSize("300x100")
+app.setResizable(canResize=False)
+app.setLocation("CENTER")
+app.addLabel("run_l", "Not Running", colspan=2, rowspan=1)
+app.setLabelBg("run_l", "orange")
+app.startFrame("LEFT", row=1, column=0)
+app.addButton('Start', pressed)
+app.stopFrame()
+app.startFrame("RIGHT", row=1, column=1)
+app.addButton('Stop', pressed)
+app.stopFrame()
+running = False
+app.go()
